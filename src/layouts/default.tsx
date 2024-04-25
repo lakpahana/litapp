5a5d19e0ef30f17e6cd0a4e364792b46a42fb987
AGS Lakpahana <agslakpahana@gmail.com>
2024-04-25 13:52:41 +0530

    Update header title in default layout component

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -56,7 +56,7 @@ export const DefaultLayout: FunctionComponent<PropsWithChildren<DefaultLayoutPro
             {/* <div className="container"> */}
                 <div className="header-title">
                     <h1>
-                        Book Store
+                        LitApp
                     </h1>
                 </div>
                 {
