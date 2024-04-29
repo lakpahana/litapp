77657f48e70f48292d876895c48907233a87cdc2
AGS Lakpahana <agslakpahana@gmail.com>
2024-04-29 07:13:38 +0530

    test

--- src/components/book-store.tsx
+++ src/components/book-store.tsx
@@ -150,176 +150,179 @@ export const BookStore: FunctionComponent = (): ReactElement => {
 
  
     return (
-        <>
-           <div className="container">
-      <div className="add-update-delete">
-        <div className="add-book">
-        <h2>Add Book</h2>
-          <label>ID:</label>
-          <input
-            type="number"
-            value={newBook.id}
-            onChange={e => setNewBook({ ...newBook, id: parseInt(e.target.value) })}
-            placeholder="ID"
-          />
-          <label>Title:</label>
-          <input
-            type="text"
-            value={newBook.book_title}
-            onChange={e => setNewBook({ ...newBook, book_title: e.target.value })}
-            placeholder="Title"
-          />
-          <label>Author:</label>
-          <input
-            type="text"
-            value={newBook.author}
-            onChange={e => setNewBook({ ...newBook, author: e.target.value })}
-            placeholder="Author"
-          />
-          <label>Category:</label>
-           <input
-            type="text"
-            value={newBook.category}
-            onChange={e => setNewBook({ ...newBook, category: e.target.value })}
-            placeholder="Category"
-          />
-          <label>Number of Copies:</label>
-          <input
-            type="number"
-            value={newBook.copies_in_stock}
-            onChange={e => setNewBook({ ...newBook, copies_in_stock: parseInt(e.target.value) })}
-            placeholder="Number of Copies"
-          />
-          <label>LKR:</label>
-          <input
-            type="number"
-            value={newBook.price}
-            onChange={e => setNewBook({ ...newBook, price: parseInt(e.target.value) })}
-            placeholder="LKR"
-          />
-          <label>Year:</label>
-          <input
-            type="number"
-            value={newBook.published_year}
-            onChange={e => setNewBook({ ...newBook, published_year: parseInt(e.target.value) })}
-            placeholder="Year"
-          />
-          <button onClick={addBook}>Add Book</button>
-        </div>
-        <div className="get-book">
-          <h2>Get Book By ID</h2>
-          <label>ID:</label>
-          <input
-            type="number"
-            value={bookIdToGet}
-            onChange={e => setBookIdToGet(parseInt(e.target.value))}
-            placeholder="Book ID"
-          />
-          <button onClick={getBookById}>Get Book</button>
-          {selectedBook && (
-            <div>
-              <h3>{selectedBook.book_title}</h3>
-              <p>Author: {selectedBook.author}</p>
-              <p>Category: {selectedBook.category}</p>
-              <p>Published Year: {selectedBook.published_year}</p>
-              <p>Price: {selectedBook.price}</p>
-              <p>Copies in Stock: {selectedBook.copies_in_stock}</p>
-            </div>
-          )}
-        </div>
-        <div className="update-book">
-          <h2>Update Book</h2>
-          <label>ID:</label>
-          <input
-            type="number"
-            value={updatedBook.id}
-            onChange={e => setUpdatedBook({ ...updatedBook, id: parseInt(e.target.value) })}
-            placeholder="ID"
-          />
-          <label>Title:</label>
-          <input
-            type="text"
-            value={updatedBook.book_title}
-            onChange={e => setUpdatedBook({ ...updatedBook, book_title: e.target.value })}
-            placeholder="Title"
-          />
-          <label>Author:</label>
-          <input
-            type="text"
-            value={updatedBook.author}
-            onChange={e => setUpdatedBook({ ...updatedBook, author: e.target.value })}
-            placeholder="Author"
-          />
-           <label>Category:</label>
-           <input
-            type="text"
-            value={updatedBook.category}
-            onChange={e => setUpdatedBook({ ...updatedBook, category: e.target.value })}
-            placeholder="Category"
-          />
-          <label>Number of Copies:</label>
-          <input
-            type="number"
-            value={updatedBook.copies_in_stock}
-            onChange={e => setUpdatedBook({ ...updatedBook, copies_in_stock: parseInt(e.target.value) })}
-            placeholder="Number of Copies"
-          />
-          <label>LKR:</label>
-          <input
-            type="number"
-            value={updatedBook.price}
-            onChange={e => setUpdatedBook({ ...updatedBook, price: parseInt(e.target.value) })}
-            placeholder="LKR"
-          />
-          <label>Year:</label>
-          <input
-            type="number"
-            value={updatedBook.published_year}
-            onChange={e => setUpdatedBook({ ...updatedBook, published_year: parseInt(e.target.value) })}
-            placeholder="Year"
-          />
-          <button onClick={updateBook}>Update Book</button>
-        </div>
-        <div className="delete-book">
-          <h2>Delete Book</h2>
-          <label>ID:</label>
-          <input
-            type="number"
-            value={bookIdToDelete}
-            onChange={e => setBookIdToDelete(parseInt(e.target.value))}
-            placeholder="ID"
-          />
-          <button onClick={deleteBook}>Delete Book</button>
-        </div>
-      </div>
-      <h1>Books</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Category</th>
-            <th>Published Year</th>
-            <th>Price</th>
-            <th>Copies in Stock</th>
-          </tr>
-        </thead>
-        <tbody>
-          {books.map(book => (
-            <tr key={book.id}>
-              <td>{book.id}</td>
-              <td>{book.book_title}</td>
-              <td>{book.author}</td>
-              <td>{book.category}</td>
-              <td>{book.published_year}</td>
-              <td>{book.price}</td>
-              <td>{book.copies_in_stock}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-        </>
+      <>
+      Loading ..
+      </>
+    //     <>
+    //        <div className="container">
+    //   <div className="add-update-delete">
+    //     <div className="add-book">
+    //     <h2>Add Book</h2>
+    //       <label>ID:</label>
+    //       <input
+    //         type="number"
+    //         value={newBook.id}
+    //         onChange={e => setNewBook({ ...newBook, id: parseInt(e.target.value) })}
+    //         placeholder="ID"
+    //       />
+    //       <label>Title:</label>
+    //       <input
+    //         type="text"
+    //         value={newBook.book_title}
+    //         onChange={e => setNewBook({ ...newBook, book_title: e.target.value })}
+    //         placeholder="Title"
+    //       />
+    //       <label>Author:</label>
+    //       <input
+    //         type="text"
+    //         value={newBook.author}
+    //         onChange={e => setNewBook({ ...newBook, author: e.target.value })}
+    //         placeholder="Author"
+    //       />
+    //       <label>Category:</label>
+    //        <input
+    //         type="text"
+    //         value={newBook.category}
+    //         onChange={e => setNewBook({ ...newBook, category: e.target.value })}
+    //         placeholder="Category"
+    //       />
+    //       <label>Number of Copies:</label>
+    //       <input
+    //         type="number"
+    //         value={newBook.copies_in_stock}
+    //         onChange={e => setNewBook({ ...newBook, copies_in_stock: parseInt(e.target.value) })}
+    //         placeholder="Number of Copies"
+    //       />
+    //       <label>LKR:</label>
+    //       <input
+    //         type="number"
+    //         value={newBook.price}
+    //         onChange={e => setNewBook({ ...newBook, price: parseInt(e.target.value) })}
+    //         placeholder="LKR"
+    //       />
+    //       <label>Year:</label>
+    //       <input
+    //         type="number"
+    //         value={newBook.published_year}
+    //         onChange={e => setNewBook({ ...newBook, published_year: parseInt(e.target.value) })}
+    //         placeholder="Year"
+    //       />
+    //       <button onClick={addBook}>Add Book</button>
+    //     </div>
+    //     <div className="get-book">
+    //       <h2>Get Book By ID</h2>
+    //       <label>ID:</label>
+    //       <input
+    //         type="number"
+    //         value={bookIdToGet}
+    //         onChange={e => setBookIdToGet(parseInt(e.target.value))}
+    //         placeholder="Book ID"
+    //       />
+    //       <button onClick={getBookById}>Get Book</button>
+    //       {selectedBook && (
+    //         <div>
+    //           <h3>{selectedBook.book_title}</h3>
+    //           <p>Author: {selectedBook.author}</p>
+    //           <p>Category: {selectedBook.category}</p>
+    //           <p>Published Year: {selectedBook.published_year}</p>
+    //           <p>Price: {selectedBook.price}</p>
+    //           <p>Copies in Stock: {selectedBook.copies_in_stock}</p>
+    //         </div>
+    //       )}
+    //     </div>
+    //     <div className="update-book">
+    //       <h2>Update Book</h2>
+    //       <label>ID:</label>
+    //       <input
+    //         type="number"
+    //         value={updatedBook.id}
+    //         onChange={e => setUpdatedBook({ ...updatedBook, id: parseInt(e.target.value) })}
+    //         placeholder="ID"
+    //       />
+    //       <label>Title:</label>
+    //       <input
+    //         type="text"
+    //         value={updatedBook.book_title}
+    //         onChange={e => setUpdatedBook({ ...updatedBook, book_title: e.target.value })}
+    //         placeholder="Title"
+    //       />
+    //       <label>Author:</label>
+    //       <input
+    //         type="text"
+    //         value={updatedBook.author}
+    //         onChange={e => setUpdatedBook({ ...updatedBook, author: e.target.value })}
+    //         placeholder="Author"
+    //       />
+    //        <label>Category:</label>
+    //        <input
+    //         type="text"
+    //         value={updatedBook.category}
+    //         onChange={e => setUpdatedBook({ ...updatedBook, category: e.target.value })}
+    //         placeholder="Category"
+    //       />
+    //       <label>Number of Copies:</label>
+    //       <input
+    //         type="number"
+    //         value={updatedBook.copies_in_stock}
+    //         onChange={e => setUpdatedBook({ ...updatedBook, copies_in_stock: parseInt(e.target.value) })}
+    //         placeholder="Number of Copies"
+    //       />
+    //       <label>LKR:</label>
+    //       <input
+    //         type="number"
+    //         value={updatedBook.price}
+    //         onChange={e => setUpdatedBook({ ...updatedBook, price: parseInt(e.target.value) })}
+    //         placeholder="LKR"
+    //       />
+    //       <label>Year:</label>
+    //       <input
+    //         type="number"
+    //         value={updatedBook.published_year}
+    //         onChange={e => setUpdatedBook({ ...updatedBook, published_year: parseInt(e.target.value) })}
+    //         placeholder="Year"
+    //       />
+    //       <button onClick={updateBook}>Update Book</button>
+    //     </div>
+    //     <div className="delete-book">
+    //       <h2>Delete Book</h2>
+    //       <label>ID:</label>
+    //       <input
+    //         type="number"
+    //         value={bookIdToDelete}
+    //         onChange={e => setBookIdToDelete(parseInt(e.target.value))}
+    //         placeholder="ID"
+    //       />
+    //       <button onClick={deleteBook}>Delete Book</button>
+    //     </div>
+    //   </div>
+    //   <h1>Books</h1>
+    //   <table>
+    //     <thead>
+    //       <tr>
+    //         <th>ID</th>
+    //         <th>Title</th>
+    //         <th>Author</th>
+    //         <th>Category</th>
+    //         <th>Published Year</th>
+    //         <th>Price</th>
+    //         <th>Copies in Stock</th>
+    //       </tr>
+    //     </thead>
+    //     <tbody>
+    //       {books.map(book => (
+    //         <tr key={book.id}>
+    //           <td>{book.id}</td>
+    //           <td>{book.book_title}</td>
+    //           <td>{book.author}</td>
+    //           <td>{book.category}</td>
+    //           <td>{book.published_year}</td>
+    //           <td>{book.price}</td>
+    //           <td>{book.copies_in_stock}</td>
+    //         </tr>
+    //       ))}
+    //     </tbody>
+    //   </table>
+    // </div>
+    //     </>
     );
 };
